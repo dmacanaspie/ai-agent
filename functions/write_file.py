aee963a567057fcb5156272a1386306357074f6a
dmacanaspie <duane.macanaspie@gmail.com>
2025-07-11 08:18:16 -0400

    write_file.py

--- functions/write_file.py
+++ functions/write_file.py
@@ -1,10 +1,10 @@
 import os
 
 def write_file(working_directory, file_path, content):
-    abs_file_path = os.path.abspath(working_directory, file_path)
+    abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
     abs_working_dir = os.path.abspath(working_directory)
     if not abs_file_path.startswith(abs_working_dir):
-        f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot write to "{file_path}" as it is outside the permitted working directory'
     try:
         if not os.path.exists(abs_file_path):
             with open(file_path, "x") as f:
