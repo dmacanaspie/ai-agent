d5a0af4ce933e953fdc7a8ae2ac7d89c186e5fa7
dmacanaspie <duane.macanaspie@gmail.com>
2025-07-15 20:51:23 -0400

    get_file_content

--- functions/get_file_content.py
+++ functions/get_file_content.py
@@ -1,7 +1,6 @@
 import os
 from functions.get_files_info import get_files_info
 from config import CHAR_LIMIT
-from google import genai
 
 
 def get_file_content(working_directory, file_path):
@@ -11,6 +10,7 @@ def get_file_content(working_directory, file_path):
 
     abs_working_dir = os.path.abspath(working_directory)
     abs_file_path = os.path.abspath(os.path.join(working_directory, file_path))
+
     if not abs_file_path.startswith(abs_working_dir):
         return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory'
     if not os.path.isfile(abs_file_path):
@@ -26,24 +26,3 @@ def get_file_content(working_directory, file_path):
 
     except Exception as e:
         return f'Error reading file "{file_path}": {e}'
-
-
-schema_get_files_info = genai.types.FunctionDeclaration(
-    name="get_files_info",
-    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
-    parameters=genai.types.Schema(
-        type=genai.types.Type.OBJECT,
-        properties={
-            "directory": genai.types.Schema(
-                type=genai.types.Type.STRING,
-                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
-            ),
-        },
-    ),
-)
-
-available_functions = genai.types.Tool(
-    function_declarations=[
-        schema_get_files_info,
-    ]
-)
